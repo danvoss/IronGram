9c32bfee36f64544d107cb3316b9e0942524a410
Dan <danielandrewvoss@gmail.com>
2016-06-28 12:31:37 -0400

    Upload functionality

--- src/main/java/com/dvoss/controllers/IronGramRestController.java
+++ src/main/java/com/dvoss/controllers/IronGramRestController.java
@@ -1,9 +1,17 @@
 package com.dvoss.controllers;
 
+import com.dvoss.entities.Photo;
+import com.dvoss.entities.User;
 import com.dvoss.services.PhotoRepository;
 import com.dvoss.services.UserRepository;
+import com.dvoss.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Dan on 6/28/16.
@@ -14,4 +22,33 @@ public class IronGramRestController {
     UserRepository users;
     @Autowired
     PhotoRepository photos;
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public User login(@RequestBody User user, HttpSession session) throws Exception {
+        User userFromDB = users.findFirstByName(user.getName());
+        if (userFromDB == null) {
+            user.setPassword(PasswordStorage.createHash(user.getPassword()));
+            users.save(user);
+        }
+        else if (!PasswordStorage.verifyPassword(user.getPassword(), userFromDB.getPassword())) {
+            throw new Exception("Wrong password.");
+        }
+        session.setAttribute("username", user.getName());
+        return user;
+    }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+    public void logout(HttpSession session) {
+        session.invalidate();
+    }
+
+    @RequestMapping(path = "/photos", method = RequestMethod.GET)
+    public Iterable<Photo> getPhotos(HttpSession session) {
+        String username = (String) session.getAttribute("username");
+        User user = users.findFirstByName(username);
+        return photos.findByRecipient(user);
+    }
+
+
+
 }
--- src/main/java/com/dvoss/services/PhotoRepository.java
+++ src/main/java/com/dvoss/services/PhotoRepository.java
@@ -1,10 +1,12 @@
 package com.dvoss.services;
 
 import com.dvoss.entities.Photo;
+import com.dvoss.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by Dan on 6/28/16.
  */
 public interface PhotoRepository extends CrudRepository<Photo, Integer> {
+    public Iterable<Photo> findByRecipient(User recipient);
 }
